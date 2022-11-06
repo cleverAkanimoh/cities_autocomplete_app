22607c34754b2c86a12ebb25b906bd2a94bd8396
Clever AKANIMOH <crushclever1@gmail.com>
2022-11-07 00:45:43 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,37 @@ const search = document.getElementById('search');
 const resultArea = document.getElementById('resultArea');
 
 const searchEntry = async value => {
-    const res = await fetch('data.json');
-    const data = await res.json();
+    let res = await fetch('data.json');
+    let datas = await res.json();
     
-    let matches = data.filter(state => {
+    let matches = datas.filter(data => {
         const regex = new RegExp(`^${value}`, 'gi');
-        return data.name.match(regex) || data.code.match(regex)
-    })
+        return data.name.match(regex) || data.code.match(regex);
+    });
+
+    if (value.length === 0) {
+        matches = [];
+        resultArea.innerHTML = '<p class="text-danger">Search result will appear here...</p>';
+    }
+
+    outputHTML(matches);
+};
+
+const outputHTML = matches => {
+    if ( matches.length > 0 ) {
+        const html = matches.map( match =>
+            `<div class="card card-body mb-3 bg-dark">
+                <h4>${match.name} (${match.code}) <span class="text-primary">${match.capital}</span></h4>
+                <p>LGAs</p>
+                <ul>
+                    <li>${match.lgas}</li>
+                </ul>
+            </div>`
+        )
+        .join('');
+        
+        resultArea.innerHTML = html;
+    }
 }
 
-search.oninput = searchEntry(search.value);
+search.oninput = () => searchEntry(search.value);
